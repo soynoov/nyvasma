1b6884251e674eae5bbd007f6271b61a326ef25f
Soy_NooV <heroyi3000@gmail.com>
2025-02-19 18:57:07 +0100

    v_0.0.4:1902

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,50 @@
-import { defineDb } from 'astro:db';
+import { reference } from 'astro:content';
+import { column, defineTable, defineDb } from 'astro:db';
 
 // https://astro.build/db/config
+
+// TABLAS
+// Jugadores
+const Jugadores = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    nombre: column.text(),
+    raza: column.text({ references: () => Razas.columns.nombre }), // FK
+    clase: column.text({ references: () => Clases.columns.nombre }), //FK
+    alineamiento: column.text(), //FK
+    trasfondo: column.text(), //FK
+    inspiracion: column.number(),
+  },
+});
+
+// -- Razas
+const Razas = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    nombre: column.text({ unique: true }),
+    velocidad: column.number(),
+  },
+});
+
+// -- Clases
+const Clases = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    nombre: column.text({ unique: true }),
+    dados_golpe: column.number(),
+  },
+});
+
+// -- Trasfondos
+const Trasfondos = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    nombre: column.text({ unique: true }),
+    competencias: column.json(),
+  },
+});
+
+// DB
 export default defineDb({
-  tables: {}
+  tables: { Jugadores, Razas, Clases },
 });
